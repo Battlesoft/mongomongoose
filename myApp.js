be1e2c43524c730c89ba3c831c484498eb4e761c
Battlesoft <yeraysantanac@gmail.com>
2024-01-22 21:58:07 +0000

    inicio 1.0

--- myApp.js
+++ myApp.js
@@ -3,7 +3,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
+
 let Person;
+
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
