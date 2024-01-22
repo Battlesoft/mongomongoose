be273722636e8fc65b3092aa4df1c5865f5d2be7
Battlesoft <yeraysantanac@gmail.com>
2024-01-22 22:01:23 +0000

    inicio 1.1

--- myApp.js
+++ myApp.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 
-let Person;
 
 
 const personSchema = new mongoose.Schema({
@@ -12,6 +11,9 @@ const personSchema = new mongoose.Schema({
   age: Number,
   favoriteFoods: [String]
 });
+
+const Person = mongoose.model("Person", personSchema);
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
