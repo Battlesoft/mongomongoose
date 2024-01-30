f40460613c16ecb4d29e71eeef4a8dce8934c876
Canario <yeraysantanac@gmail.com>
2024-01-30 18:40:08 +0000

    2.03

--- myApp.js
+++ myApp.js
@@ -5,6 +5,12 @@ const { Schema } = mongoose;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const conn = mongoose.connection
+conn.on('connected', function(){
+console.log("mongodb启动成功")
+})
+
+
 const personSchema = new Schema({
   name: {
     type: String,
